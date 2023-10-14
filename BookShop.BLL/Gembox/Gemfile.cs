e65a5ebc6969534d7286ee4c35b194dbec20949a
Hoanh Lo <hoanhlvph24903@fpt.edu.vn>
2023-10-14 21:09:24 +0700

    up

---
+++ BookShop.BLL/Gembox/Gemfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShop.BLL.IService;
+using BookShop.BLL.Service;
+using BookShop.DAL.Entities;
+//using GemBox.Spreadsheet;
+using Microsoft.Data.SqlClient;
+
+namespace BookShop.BLL.Gembox
+{
+    public class Gemfile
+    {
+        public IOrderService _IorderService;
+        public IOrderDetailService _Iorderdetailservice;
+        
+        public Gemfile()
+        {
+           // _IorderService = new OrderService();
+            _Iorderdetailservice = new OrderDetailService();
+            
+        }
+       public void GetExel()
+        {
+           
+        }
+    }
+}
