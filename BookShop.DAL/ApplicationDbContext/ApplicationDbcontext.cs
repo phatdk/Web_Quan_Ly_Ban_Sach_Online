1e6313e48d38c252d6a9958ed6845301b0523569
KhanhDangHocTiengAnh <133343354+KhanhDangHocTiengAnh@users.noreply.github.com>
2023-11-16 19:42:44 +0700

    up

--- BookShop.DAL/ApplicationDbContext/ApplicationDbcontext.cs
+++ BookShop.DAL/ApplicationDbContext/ApplicationDbcontext.cs
@@ -58,7 +58,7 @@ namespace BookShop.DAL.ApplicationDbContext
             if (!optionsBuilder.IsConfigured)
             {
                 //optionsBuilder.UseSqlServer("Data Source=DESKTOP-21G560I\\SQLEXPRESS;Initial Catalog=BookShop.Datn2;Integrated Security=True");
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-21G560I\SQLEXPRESS;Initial Catalog=BookShop.Datn;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-21G560I\\SQLEXPRESS;Initial Catalog=BookShop.Datn;Integrated Security=True");
                 //  optionsBuilder.UseSqlServer(@"Data Source=PHUCDEPZAI2003\SQLEXPRESS;Initial Catalog=BookShopDatn;Integrated Security=True;Encrypt=False");
             }
         }
