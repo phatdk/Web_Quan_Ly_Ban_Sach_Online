2549b246639ec3f52afb3f5b4558e532a287d10f
PhucPh3005 <phuc2003zgt@gmail.com>
2024-01-11 21:55:54 +0700

    update code blog 11.1

--- BookShop.BLL/ConfigurationModel/NewsModel/CreateNewsModel.cs
+++ BookShop.BLL/ConfigurationModel/NewsModel/CreateNewsModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,16 @@ using System.Threading.Tasks;
 
 namespace BookShop.BLL.ConfigurationModel.NewsModel
 {
-	public class CreateNewsModel
-	{
-		public string Title { get; set; }
-		public string Content { get; set; }
-		public string? Description { get; set; }
-		public int Status { get; set; }
-		public string Img { get; set; }
-		public DateTime DateTime { get; set; }
-	}
+    public class CreateNewsModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Content { get; set; }
+        public string? Description { get; set; }
+        public int Status { get; set; }
+        public string? Img { get; set; }
+        public DateTime DateTime { get; set; }
+        public IFormFile imageFile { get; set; }
+
+    }
 }
